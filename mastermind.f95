f285933cb9027c85dd89ccc52edf7f940d4ff034
Erik Andersson <dv13ean@cs.umu.se>
2016-10-18 01:30:02 +0200

    added printing pegs of all colours

--- mastermind.f95
+++ mastermind.f95
@@ -1,3 +1,25 @@
+subroutine print_colour(colour)
+	integer :: colour
+	write(*, '(A, I0, A)', advance='no') ,achar(27)//'[1;',colour,'m*'//&
+	&achar(27)//'[0m'
+end subroutine print_colour
+
 program MASTERMIND
-    stop
-end
+	implicit none
+	print*,'testing colours'
+	print*,achar(27)//'[1;31mRED'
+	print*,achar(27)//'[1;32mGREEN'
+	print*,achar(27)//'[1;33mYELLOW'
+	print*,achar(27)//'[1;34mBLUE'
+	print*,achar(27)//'[1;35mPURPLE'
+	print*,achar(27)//'[1;36mCYAN'
+	print*,achar(27)//'[0m' !reset the colours
+	print*,'input is now normal'
+	call print_colour(31)
+	call print_colour(32)
+	call print_colour(33)
+	call print_colour(34)
+	call print_colour(35)
+	call print_colour(36)
+	print*,'' !empty newline
+end program MASTERMIND
