f25bea9ba8021beb735e8a21304ad8f5c30ba168
Erik Andersson <dv13ean@cs.umu.se>
2016-10-19 01:53:50 +0200

    moved code generation to subroutine

--- mastermind.f95
+++ mastermind.f95
@@ -1,41 +1,48 @@
-subroutine print_colour(colour)
-	integer :: colour
-	write(*, '(A, I0, A)', advance='no') ,achar(27)//'[1;',colour,'m*'//&
-	&achar(27)//'[0m'
-end subroutine print_colour
+SUBROUTINE print_colour(colour)
+	INTEGER :: colour
+	WRITE(*, '(A, I0, A)', ADVANCE='NO') ,ACHAR(27)//'[1;',colour,'m*'//&
+	&ACHAR(27)//'[0m'
+END SUBROUTINE print_colour
 
-program MASTERMIND
-	implicit none
-	INTEGER, PARAMETER           :: num_pegs = 4
-	INTEGER                      :: i, time
-	INTEGER, DIMENSION(num_pegs) :: code
-	REAL                         :: r(num_pegs)
-	INTEGER, ALLOCATABLE         :: seed(:)
+SUBROUTINE get_code(num_pegs, code)
+	INTEGER, INTENT(in)                       :: num_pegs
+	INTEGER, DIMENSION(num_pegs), INTENT(out) :: code
+	INTEGER, ALLOCATABLE                      :: seed(:)
+	INTEGER                                   :: seed_size, time, i
+	REAL                                      :: r(num_pegs)
 	
-	call RANDOM_SEED()
-	call RANDOM_SEED(size = i)
-	allocate(seed(i))
-	call RANDOM_SEED(get = seed)
-	call SYSTEM_CLOCK(count = time)
+	CALL RANDOM_SEED()
+	CALL RANDOM_SEED(size = seed_size)
+	ALLOCATE(seed(seed_size))
+	CALL RANDOM_SEED(get = seed)
+	CALL SYSTEM_CLOCK(count = time)
 	seed = time
-	call RANDOM_SEED(put = seed)
-	call RANDOM_NUMBER(r)
-	
-	print*,'testing colours'
-	print*,achar(27)//'[1;31mRED'
-	print*,achar(27)//'[1;32mGREEN'
-	print*,achar(27)//'[1;33mYELLOW'
-	print*,achar(27)//'[1;34mBLUE'
-	print*,achar(27)//'[1;35mPURPLE'
-	print*,achar(27)//'[1;36mCYAN'
-	print*,achar(27)//'[0m' !reset the colours
-	print*,'input is now normal'
-	
-	do i=1,num_pegs
+	CALL RANDOM_SEED(put = seed)
+	CALL RANDOM_NUMBER(r)
+	DEALLOCATE(seed)
+	DO i=1,num_pegs
 		code(i) = NINT(r(i)*5)+31
-		call print_colour(code(i))
-	end do
+	END DO
+END SUBROUTINE get_code
+
+PROGRAM MASTERMIND
+	IMPLICIT NONE
+	INTEGER, PARAMETER           :: num_pegs = 4
+	INTEGER                      :: i
+	INTEGER, DIMENSION(num_pegs) :: code
 	
-	print*,'' !empty newline
-	deallocate(seed)
-end program MASTERMIND
+	PRINT*,'testing colours'
+	PRINT*,achar(27)//'[1;31mRED'
+	PRINT*,achar(27)//'[1;32mGREEN'
+	PRINT*,achar(27)//'[1;33mYELLOW'
+	PRINT*,achar(27)//'[1;34mBLUE'
+	PRINT*,achar(27)//'[1;35mPURPLE'
+	PRINT*,achar(27)//'[1;36mCYAN'
+	PRINT*,achar(27)//'[0m' !reset the colours
+	PRINT*,'input is now normal'
+	CALL get_code(num_pegs, code)
+	DO i=1,num_pegs
+		CALL print_colour(code(i))
+	END DO
+	PRINT*,'' !empty newline
+END PROGRAM MASTERMIND
