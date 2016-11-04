588bb44542e5130634cfe234cb422e719b4fcaf5
Erik Andersson <dv13ean@cs.umu.se>
2016-11-04 14:22:46 +0100

    made subroutine to get one guess

--- mastermind.f95
+++ mastermind.f95
@@ -25,11 +25,51 @@ SUBROUTINE get_code(num_pegs, code)
 	END DO
 END SUBROUTINE get_code
 
+SUBROUTINE get_guess(num_pegs, guess)
+	INTEGER, INTENT(in)                       :: num_pegs
+	INTEGER, DIMENSION(num_pegs), INTENT(out) :: guess
+	INTEGER                                   :: i, selected
+
+	i=1
+	DO
+		IF (i > num_pegs) EXIT
+		WRITE(*, '(A, I0)'), 'Pick colour for peg #', i
+		PRINT *, '1. '//achar(27)//'[1;31mRED'//    achar(27)//'[0m'
+		PRINT *, '2. '//achar(27)//'[1;32mGREEN'//  achar(27)//'[0m'
+		PRINT *, '3. '//achar(27)//'[1;33mYELLOW'// achar(27)//'[0m'
+		PRINT *, '4. '//achar(27)//'[1;34mBLUE'//   achar(27)//'[0m'
+		PRINT *, '5. '//achar(27)//'[1;35mPURPLE'// achar(27)//'[0m'
+		PRINT *, '6. '//achar(27)//'[1;36mCYAN'//   achar(27)//'[0m'
+
+		READ(*,*), selected
+		SELECT CASE(selected)
+			CASE(1)
+				guess(i) = 31
+			CASE(2)
+				guess(i) = 32
+			CASE(3)
+				guess(i) = 33
+			CASE(4)
+				guess(i) = 34
+			CASE(5)
+				guess(i) = 35
+			CASE(6)
+				guess(i) = 36
+			CASE DEFAULT
+				WRITE(*, '(I0, A)'), selected,' is an invalid choice.'
+				i = i - 1
+		END SELECT
+
+		i = i + 1
+	END DO
+END SUBROUTINE get_guess
+
 PROGRAM MASTERMIND
 	IMPLICIT NONE
 	INTEGER, PARAMETER           :: num_pegs = 4
 	INTEGER                      :: i
 	INTEGER, DIMENSION(num_pegs) :: code
+	INTEGER, DIMENSION(num_pegs) :: guess
 	
 	PRINT*,'testing colours'
 	PRINT*,achar(27)//'[1;31mRED'
@@ -39,10 +79,16 @@ PROGRAM MASTERMIND
 	PRINT*,achar(27)//'[1;35mPURPLE'
 	PRINT*,achar(27)//'[1;36mCYAN'
 	PRINT*,achar(27)//'[0m' !reset the colours
-	PRINT*,'input is now normal'
+
 	CALL get_code(num_pegs, code)
 	DO i=1,num_pegs
 		CALL print_colour(code(i))
+	END DO
+	PRINT*,'' !empty newline
+	CALL get_guess(num_pegs, guess)
+	!WRITE(*,"('',A1,A,$)") '',CHAR(13)
+	DO i=1,num_pegs
+		CALL print_colour(guess(i))
 	END DO
 	PRINT*,'' !empty newline
 END PROGRAM MASTERMIND
